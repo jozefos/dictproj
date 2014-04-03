74d8fe050d61480125a1ef785c6c16b29efb156c
Joseph <jozefos@hotmail.co.uk>
2014-04-03 11:49:44 +0100

    first commit

---
+++ continue.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dictionary.h"
+
+//fscanf to read words and definitions
+
+void d_initialise() {
+  //initialise the dictionary
+  struct entry {
+    
+  };
+}
---
+++ start.c
@@ -0,0 +1,15 @@
+void d_initialise() {
+  typedef struct {
+    char[]  word;
+    char[]  def;
+  } entry;
+  
+  /* array of arrays of entry's */
+  
+  /* dynamically allocate arrays as entry's are read in from the input file. */
+  
+  /* find array where entry would be stored if in the dictionary */
+  
+  /* compare search term to the words in that array */
+  
+  /* 
