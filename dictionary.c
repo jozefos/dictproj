a4386496ddde5ede68da8fdd61abf595c0eaf5eb
Joseph <jozefos@hotmail.co.uk>
2014-04-04 14:54:35 +0100

    added pseudo code to give instruction for working on this at home

--- dictionary.c
+++ dictionary.c
@@ -9,31 +9,36 @@ struct dictionary
 {
   char[]              cword;
   ht_implementation   dict;
-}
+};
 
 struct entry
 {
   char[MAX_WORD_SIZE]   word;
   char[MAX_DESC_SIZE]   desc;
+};
+
+int hashfun(struct entry e) {
+  char * word = getkeyfun(e->word);
+  //run getkeyfun, execute hash algorithm on it, return hash value
+}
+
+char* getkeyfun(struct entry e) {
+  //look at David's implementation of a hash table for a function to model this on
+  //read up on strings and char*'s
+  
+  //word is key, extract from structure
+  //return word string
 }
 
 void d_initialise() {
-  struct dictionary d;
+  struct dictionary d = malloc(//look up malloc function
   d->cword  = "";
   d->dict   = new_ht(26, hashfunc, getkeyfunc); //compare how this is done in david's examples
 }
   
-  /* array of arrays of entry's */
-  
-  /* dynamically allocate arrays as entry's are read in from the input file. */
-  
-  /* find array where entry would be stored if in the dictionary */
-  
-  /* compare search term to the words in that array */
-  
 int d_read_from_file(const char * filename) {
   char line[MAX_WORD_SIZE + MAX_DESC_SIZE];
-  while ( fgets(line,MAX_WORD_SIZE + MAX_DESC_SIZE,filename) != ".\0" )d {
+  while ( fgets(line,MAX_WORD_SIZE + MAX_DESC_SIZE,filename) != ".\0" ) {
     //hash key then place in bst along with definition;
   }
   
